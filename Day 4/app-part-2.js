d497c6cfeaae64ca2436d11f58d1b0e7ed38cce7
Aleksandrs <staubsauger@inbox.lv>
2023-12-04 23:27:24 +0200

    feat: Day 4 Part 2 (Ден разленился)

--- Day 4/app-part-2.js
+++ Day 4/app-part-2.js
@@ -7,13 +7,24 @@ const rd = readline.createInterface({
     input: fs.createReadStream('data'),
     console: false
 });
+const cardsCopies = []
+let idx = 0
 
 rd.on('line', (line) => {
     const data = line.split(/\|/);
     const winNumbers = data[0].trim().split(/\s+/).splice(1);
     const elfNumbers = data[1].trim().split(/\s+/);
-    cards.set(Number(winNumbers[0].slice(0, -1)), [winNumbers.splice(1), elfNumbers]);
+    const winnedNumbers = (winNumbers.filter(number => elfNumbers.includes(number))).length
+    if (cardsCopies[idx] === undefined) {
+        cardsCopies[idx] = 1
+    }
+    for (let i = idx +1; i < idx +1 + winnedNumbers; i++) {
+        cardsCopies[i] = cardsCopies[i] !== undefined ? cardsCopies[i] + cardsCopies[idx] : cardsCopies[idx] + 1
+    }
+    result += cardsCopies[idx]
+    idx++
 });
 
 rd.on('close', () => {
+    console.log(result)
 });
