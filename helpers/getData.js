1657384e1e2ff11664797e0569fe9268f810a3a3
Deniss Sazanovs <dittikun@gmail.com>
2023-12-09 14:22:17 +0200

    - Add response code validation

--- helpers/getData.js
+++ helpers/getData.js
@@ -1,13 +1,13 @@
 import fetch from 'node-fetch';
 import fs from 'fs/promises';
-import 'dotenv/config'
+import 'dotenv/config';
 
 const filePath = `Day ${process.argv[2]}/data`; // Specify the desired file path
 
 // Check if the file exists
 try {
     const fileContent = await fs.readFile(filePath, 'utf-8');
-    console.log('File already exist. If you want to download new file, remove previous one');
+    console.log('File already exists. If you want to download a new file, remove the previous one.');
 } catch (error) {
     // File doesn't exist, make a new request
     const response = await fetch(`https://adventofcode.com/2023/day/${process.argv[2]}/input`, {
@@ -16,6 +16,12 @@ try {
             "Cookie": `session=${process.env.COOKIE}`
         }
     });
+
+    // Check if the response status is not 200 (OK)
+    if (response.status !== 200) {
+        console.error(`Failed to fetch data. Status: ${response.status}`);
+        process.exit(1); // Exit the process with an error code
+    }
 
     const body = await response.text();
 
